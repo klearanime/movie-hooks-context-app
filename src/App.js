5a52ba7c0b20baae213766e9533dab727cd7f30a
Crystal K Young <crystal.young@codeimmersive.com>
2021-04-18 15:51:54 -0400

    app created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.input = React.createRef()
+
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+
+  }
+
+
+  render() {
+    return (
+      <div style={{ textAlign: "center" }}>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" ref={this.input} />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    )
+  }
 }
 
-export default App;
+
+
+
+
+
+
+
+
+
+// import React from "react";
+
+
+
+// const App = () => {
+//   return (
+//     <div style={{ textAlign: "center" }}>
+//       <Wrapper>
+//         <Header />
+//         <Body />
+//         <Footer />
+//       </Wrapper>
+//     </div>
+//   )
+// }
+
+// const Wrapper = (props) => {
+//   // console.log(props);
+//   return <>{props.children}</>
+// }
+
+// const Header = () => {
+//   return (
+//     <div>Header</div>
+//   )
+// }
+
+// const Body = () => {
+//   return (
+//     <div>Body</div>
+//   )
+// }
+
+// const Footer = () => {
+//   return (
+//     <div>Footer</div>
+//   )
+// }
+
+
+
+// export default App
